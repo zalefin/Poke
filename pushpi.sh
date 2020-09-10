20e44c576181cfd8acbaa8fa8fdbe158e330c196
Zach Lefin <zachlefin@gmail.com>
2020-09-10 15:39:10 -0600

    add note to pushpi

--- pushpi.sh
+++ pushpi.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# Push your local repo contents to the raspberry pi server for testing
+#
+# Note:
+# - requires pub key
+# - does not copy dot files (won't copy .git)
 scp -r * csci3308@zachlef.in:~/project/

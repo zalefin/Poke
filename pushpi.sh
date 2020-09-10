dbd0fa6420bdc0013725a9065086fd91b78df28b
Zach Lefin <zachlefin@gmail.com>
2020-09-10 15:45:38 -0600

    change pushpi

--- pushpi.sh
+++ pushpi.sh
@@ -4,4 +4,8 @@
 # Note:
 # - requires pub key
 # - does not copy dot files (won't copy .git)
+
+# Remove all existing in remote
+ssh csci3308@zachlef.in "rm -rf ~/project/*"
+# Copy local to remote
 scp -r * csci3308@zachlef.in:~/project/

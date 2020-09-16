57d5026cb050fc2e66038986b2f6aa48e25c737e
Zach Lefin <zachlefin@gmail.com>
2020-09-15 19:05:21 -0600

    add settings comment

--- src/server/pokeserver/settings.py
+++ src/server/pokeserver/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
+
+POKESERVER
 """
 
 from pathlib import Path

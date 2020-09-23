beda5c825daee8d99fd35c11d99843d823eee174
Zach Lefin <zachlefin@gmail.com>
2020-09-23 13:46:05 -0600

    change pushpi script massive speed increase

--- pushpi.sh
+++ pushpi.sh
@@ -7,5 +7,11 @@
 
 # Remove all existing in remote
 ssh csci3308@zachlef.in "rm -rf ~/project/*"
-# Copy local to remote
-scp -r * csci3308@zachlef.in:~/project/
+# Create tarball
+tar czf transfer.tar.gz *
+# Copy tarball to remote
+scp transfer.tar.gz csci3308@zachlef.in:~/project/
+# Extract and remove
+ssh csci3308@zachlef.in "cd ~/project && tar xf transfer.tar.gz && rm transfer.tar.gz"
+# Remove local
+rm transfer.tar.gz

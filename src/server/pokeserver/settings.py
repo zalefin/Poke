c3162ec772cb9d8bad406602b84d11dcdfb27a2b
Zach Lefin <zachlefin@gmail.com>
2020-09-22 23:05:01 -0600

    add register app

--- src/server/pokeserver/settings.py
+++ src/server/pokeserver/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'register',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

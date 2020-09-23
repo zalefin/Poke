0720e7fd8797b6bb707cc38c62f49e023ae0ed79
Zach Lefin <zachlefin@gmail.com>
2020-09-23 00:26:45 -0600

    add register view

--- src/server/register/views.py
+++ src/server/register/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from uuid import uuid4
+from datetime import datetime
 
-def index(request):
-    return HttpResponse("wew lad")
+from .models import User
+
+def register(request, name):
+    uuid = str(uuid4()) # generate uuid for registration
+    User.objects.create(uuid=uuid, name=name, reg_date=datetime.now()) # commit to database
+    return HttpResponse(uuid)
 

7421893e7efcd07e92eb2d346194b7a3186d69ad
Zach Lefin <zachlefin@gmail.com>
2020-09-22 22:15:39 -0600

    migrate to postgresql

--- src/server/pokeserver/settings.py
+++ src/server/pokeserver/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'pokeserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokedata',
+        'USER': 'admin',
+        'PASSWORD': 'aaa',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

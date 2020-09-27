56ed216dd930e44ba4ed14380310e008252ad686
Zach Lefin <zachlefin@gmail.com>
2020-09-27 15:14:29 -0600

    add timezone dt store

--- src/server/register/views.py
+++ src/server/register/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from uuid import uuid4
 from datetime import datetime
+import pytz
 
 from .models import User
 
 def register(request, name):
     uuid = str(uuid4()) # generate uuid for registration
-    User.objects.create(uuid=uuid, name=name, reg_date=datetime.now()) # commit to database
+    dt_naive = datetime.now()
+    tz = pytz.timezone('America/Denver') # Use timezone metadata
+    dt_aware = tz.localize(dt_naive)
+    User.objects.create(uuid=uuid, name=name, reg_date=dt_aware) # commit to database
     return HttpResponse(uuid)
 

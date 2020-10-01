005bbcfb196b8feffad9efcaabb7da7f8e330213
Zach Lefin <zachlefin@gmail.com>
2020-10-01 01:30:20 -0600

    change user page dispay fields

--- src/server/register/admin.py
+++ src/server/register/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'name', 'reg_date']

cc19def4b96a4dfb2f88e3448aa1dddcf5e3c30d
heki5656 <70242959+heki5656@users.noreply.github.com>
2020-10-02 22:48:43 -0600

    implemented friend database transaction

--- src/server/poke/views.py
+++ src/server/poke/views.py
@@ -4,17 +4,28 @@ from uuid import uuid4
 from datetime import datetime
 import pytz
 
-from .models import User
+from .models import User, Friend
+
+
+def _nowlocal():
+    dt_naive = datetime.now()
+    tz = pytz.timezone('America/Denver') # Use timezone metadata
+    return tz.localize(dt_naive)
+
 
 def register(request, name):
     uuid = str(uuid4()) # generate uuid for registration
-    dt_naive = datetime.now()
-    tz = pytz.timezone('America/Denver') # Use timezone metadata
-    dt_aware = tz.localize(dt_naive)
+    dt_aware = _nowlocal()
     User.objects.create(uuid=uuid, name=name, reg_date=dt_aware) # commit to database
     return HttpResponse(uuid)
 
 
 def add_friend(request, user, target):
-    return HttpResponse("success")
-
+    if Friend.objects.filter(user_uuid=user,friend_uuid=target).exists(): #already friends case
+        return HttpResponse("Already friends :P")
+    else:
+        dt_aware = _nowlocal() #new friends case 
+        Friend.objects.create(user_uuid=user, friend_uuid=target, added_date=dt_aware, total_pokes=0)
+        Friend.objects.create(user_uuid=target, friend_uuid=user, added_date=dt_aware, total_pokes=0)
+        return HttpResponse("success")
+    

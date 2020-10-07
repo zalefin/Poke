b7c4eaa8ee1f20bee9a0f3c1e4a71db7c9182492
Zach Lefin <zachlefin@gmail.com>
2020-10-06 19:07:51 -0600

    change method to POST only

--- src/server/poke/urls.py
+++ src/server/poke/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns= [
-        path('register/name=<name>', views.register, name='register'),
-        path('<user>/friends/add/uuid=<target>', views.add_friend, name='add_friend'),
+        path('register', views.register, name='register'),
+        path('friends/add', views.add_friend, name='add_friend'),
         ]
--- src/server/poke/views.py
+++ src/server/poke/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from uuid import uuid4
 from datetime import datetime
 import pytz
@@ -13,19 +14,38 @@ def _nowlocal():
     return tz.localize(dt_naive)
 
 
-def register(request, name):
+def _must_be_POST(f):
+    def inner(request, *args, **kwargs):
+        if request.method == 'POST':
+            return f(request, *args, **kwargs)
+        else:
+            return HttpResponse('requires POST')
+    return inner
+
+
+@csrf_exempt
+@_must_be_POST
+def register(request):
+    name = request.POST['name']
     uuid = str(uuid4()) # generate uuid for registration
     dt_aware = _nowlocal()
     User.objects.create(uuid=uuid, name=name, reg_date=dt_aware) # commit to database
     return HttpResponse(uuid)
 
 
-def add_friend(request, user, target):
+@csrf_exempt
+@_must_be_POST
+def add_friend(request):
+    user = request.POST['user']
+    target = request.POST['target']
     if Friend.objects.filter(user_uuid=user,friend_uuid=target).exists(): #already friends case
         return HttpResponse("Already friends :P")
     else:
-        dt_aware = _nowlocal() #new friends case 
-        Friend.objects.create(user_uuid=user, friend_uuid=target, added_date=dt_aware, total_pokes=0)
-        Friend.objects.create(user_uuid=target, friend_uuid=user, added_date=dt_aware, total_pokes=0)
-        return HttpResponse("success")
-    
+        if User.objects.filter(uuid=user).exists() and User.objects.filter(uuid=target).exists():
+            dt_aware = _nowlocal() #new friends case
+            Friend.objects.create(user_uuid=user, friend_uuid=target, added_date=dt_aware, total_pokes=0)
+            Friend.objects.create(user_uuid=target, friend_uuid=user, added_date=dt_aware, total_pokes=0)
+            return HttpResponse("success")
+        else:
+            return HttpResponse("invalid user")
+

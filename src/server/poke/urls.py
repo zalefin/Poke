cedc9ab60d4138c96349a2e0f287dac84f8be68f
Zach Lefin <zachlefin@gmail.com>
2020-10-13 01:10:41 -0600

    implement server handle poke

--- src/server/poke/urls.py
+++ src/server/poke/urls.py
@@ -6,4 +6,5 @@ urlpatterns= [
         path('register', views.register, name='register'),
         path('friends/add', views.add_friend, name='add_friend'),
         path('update', views.update_poll, name='update_poll'),
+        path('poke', views.do_poke_inbound, name='do_poke_inbound'),
         ]

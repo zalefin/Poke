eb5b4dd4d8358dd71a8ad94a0abee07d767f2f98
Jake <jnichols2719@protonmail.com>
2020-11-03 14:18:47 -0700

    Notifications should now open app when pressed.

--- src/PokeApp/app/src/main/java/com/example/pokeapp/NotiMan.java
+++ src/PokeApp/app/src/main/java/com/example/pokeapp/NotiMan.java
@@ -2,7 +2,9 @@ package com.example.pokeapp;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
@@ -32,11 +34,19 @@ public class NotiMan {
 
     //create notification
     public void createNotification() {
+        //create intent to open app
+        Intent openApp = new Intent(c, MainActivity.class);
+        openApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //create pending intent with openApp
+        PendingIntent onTap = PendingIntent.getActivity(c, 0, openApp, 0);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(c, CHANNEL_ID)
-                .setSmallIcon(R.drawable.qrbackgroundtest)
-                .setContentTitle("Notification")
-                .setContentText("This is a notification.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setSmallIcon(R.drawable.qrbackgroundtest) //icon
+                .setContentTitle("Notification") //notification name
+                .setContentText("This is a notification.") //notification content
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) //priority
+                .setContentIntent(onTap) //set action on tap
+                .setAutoCancel(true); //remove notification automatically
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(c);
         // notificationId is a unique int for each notification that you must define
@@ -45,11 +55,19 @@ public class NotiMan {
     }
 
     public void createNotification(String body) {
+        //create intent to open app
+        Intent openApp = new Intent(c, MainActivity.class);
+        openApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //create pending intent with openApp
+        PendingIntent onTap = PendingIntent.getActivity(c, 0, openApp, 0);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(c, CHANNEL_ID)
                 .setSmallIcon(R.drawable.qrbackgroundtest)
                 .setContentTitle("Poked!")
                 .setContentText(body)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(onTap)
+                .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(c);
         // notificationId is a unique int for each notification that you must define

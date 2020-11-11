6269e32cb6836596de90281103dcc006e9650f3d
Zach Lefin <zachlefin@gmail.com>
2020-11-10 19:50:34 -0700

    refactor RequestManager methods, err handler

--- src/PokeApp/app/src/main/java/com/example/pokeapp/RequestManager.java
+++ src/PokeApp/app/src/main/java/com/example/pokeapp/RequestManager.java
@@ -2,6 +2,8 @@ package com.example.pokeapp;
 
 import android.content.Context;
 
+import androidx.annotation.Nullable;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
@@ -20,19 +22,26 @@ public class RequestManager {
         queue = Volley.newRequestQueue(context);
     }
 
-    public static void addRegisterRequest(String name, Response.Listener<String> callback) {
+
+    public static void addRegisterRequest(
+            String name,
+            Response.Listener<String> callback,
+            @Nullable Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("name", name);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/register",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
 
-    public static void addPollRequest(String userUUID, Response.Listener<String> callback) {
+    public static void addPollRequest(
+            String userUUID,
+            Response.Listener<String> callback,
+            @Nullable Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("user", userUUID);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/poll",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
 
@@ -40,45 +49,99 @@ public class RequestManager {
             String userUUID,
             String targetUUID,
             String payload,
-            Response.Listener<String> callback) {
+            Response.Listener<String> callback,
+            @Nullable Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("user", userUUID);
         params.put("target", targetUUID);
         params.put("payload", payload);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/poke",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
 
-    public static void addUpdateRequest(String userUUID, Response.Listener<String> callback) {
+    public static void addUpdateRequest(
+            String userUUID,
+            Response.Listener<String> callback,
+            Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("user", userUUID);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/update",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
 
     public static void addAddFriendRequest(
             String userUUID,
             String targetUUID,
-            Response.Listener<String> callback) {
+            Response.Listener<String> callback,
+            Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("user", userUUID);
         params.put("target", targetUUID);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/friends/add",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
 
     public static void addRemoveFriendRequest(
             String userUUID,
             String targetUUID,
-            Response.Listener<String> callback) {
+            Response.Listener<String> callback,
+            Response.ErrorListener errorListener) {
         final Map<String, String> params = new HashMap<>();
         params.put("user", userUUID);
         params.put("target", targetUUID);
         PokeRequest request = new PokeRequest(BASE_URL + "poke/friends/delete",
-                callback, null, params);
+                callback, errorListener, params);
         queue.add(request);
     }
+
+    /*
+     * BEGIN NO ERR HANDLE OVERLOADS
+     */
+
+    public static void addRegisterRequest(
+            String name,
+            Response.Listener<String> callback) {
+        addRegisterRequest(name, callback, null);
+    }
+
+    public static void addPollRequest(
+            String userUUID,
+            Response.Listener<String> callback) {
+        addPollRequest(userUUID, callback, null);
+    }
+
+    public static void addPokeRequest(
+            String userUUID,
+            String targetUUID,
+            String payload,
+            Response.Listener<String> callback) {
+        addPokeRequest(userUUID, targetUUID, payload, callback, null);
+    }
+
+    public static void addUpdateRequest(
+            String userUUID,
+            Response.Listener<String> callback) {
+        addUpdateRequest(userUUID, callback, null);
+    }
+
+    public static void addAddFriendRequest(
+            String userUUID,
+            String targetUUID,
+            Response.Listener<String> callback) {
+        addAddFriendRequest(userUUID, targetUUID, callback, null);
+    }
+
+    public static void addRemoveFriendRequest(
+            String userUUID,
+            String targetUUID,
+            Response.Listener<String> callback) {
+        addRemoveFriendRequest(userUUID, targetUUID, callback, null);
+    }
+
+    /*
+     * END NO ERR HANDLE OVERLOADS
+     */
 }

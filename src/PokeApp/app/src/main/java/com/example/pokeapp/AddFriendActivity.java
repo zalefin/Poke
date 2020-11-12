df4098c4f68afe4189a30dda9a8e56080163e533
graefmf <magr7489@colorado.edu>
2020-11-11 18:42:06 -0700

    Refactor QR Generation to write and load bmp from file

--- src/PokeApp/app/src/main/java/com/example/pokeapp/AddFriendActivity.java
+++ src/PokeApp/app/src/main/java/com/example/pokeapp/AddFriendActivity.java
@@ -1,9 +1,12 @@
 package com.example.pokeapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +20,14 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 
 public class AddFriendActivity extends AppCompatActivity{
 
@@ -26,18 +37,25 @@ public class AddFriendActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_add_friend);
+        fileManager = new FileMan(this);
+
         //finds image view and sets image to generated bitmap
         ImageView qrView = (ImageView) findViewById(R.id.qrView);
-
-        fileManager = new FileMan(this);
-        Bitmap loadedBmp = createQrCodeFromUUID(fileManager.getUUID());
-        //If no valid UUID exists, display no valid UUID
-        TextView noValidUUID = (TextView) findViewById(R.id.noQrCode);
         TextView uuidText = (TextView) findViewById(R.id.uuidViewQR);
+
+        //attempts to read bitmap from file, if none exist returns null
+        Bitmap loadedBmp = readBitmapFromFile("qrcode.png");
+
+        //if no qr code is currently saved, generates new qr code
         if(loadedBmp == null) {
-            noValidUUID.setText("No Valid UUID Loaded");
+            Log.i("QR", "Creating Bitmap");
+            Bitmap outBmp = createQrCodeFromUUID(fileManager.getUUID());
+            outBmp = changeColor(outBmp, 0x7CFC00);
+            writeBitmapToFile("qrcode.png", outBmp);
+            //reload page
+            this.finish();
+            startActivity(this.getIntent());
         }else{
             //gets UUID from fileman
             uuidText.setText(fileManager.getName() + "\n" + fileManager.getUUID());
@@ -59,6 +77,54 @@ public class AddFriendActivity extends AppCompatActivity{
             e.printStackTrace();
         }
         return null;
+    }
+
+    //changes background color of qr code
+    private Bitmap changeColor(Bitmap input, int color){
+        if(input == null){
+            return null;
+        }
+        int width = input.getWidth();
+        int height = input.getHeight();
+        int [] pixels = new int[height * width];
+        input.getPixels(pixels, 0, width, 0 ,0, width, height);
+        for(int i = 0; i < pixels.length; i++){
+            if(pixels[i] == -1){
+                pixels[i] = color;
+            }
+        }
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        output.setPixels(pixels, 0, width,0,0,width, height);
+        return output;
+    }
+
+    //writes generated qr to file
+    private void writeBitmapToFile(String filename, Bitmap outBmp){
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        outBmp.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+        FileOutputStream fos;
+        try {
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(bytes.toByteArray());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //attempts to read existing qr from file
+    private Bitmap readBitmapFromFile(String filename){
+        Bitmap outBmp;
+        FileInputStream fis;
+        try {
+            fis = openFileInput(filename);
+            outBmp = BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return outBmp;
     }
 
     public void leaveQR(View v){

eae9ebb01ca3fed2d3cd9f38c5936e080557c6fd
Zach Lefin <zachlefin@gmail.com>
2020-12-07 13:42:16 -0700

    move script behavior into funcs

--- tests/add_dummy_friends.py
+++ tests/add_dummy_friends.py
@@ -2,13 +2,22 @@
 from uuid import UUID
 from threading import Thread
 from cli_client import Client
-u = input('Your UUID> ')
-n = int(input('Number of friends to generate/add> '))
-clients = [Client(None, True, 'poke.zachlef.in') for _ in range(n)]
-def act(client, name, friend):
-    client.register(name)
-    client.add_friend(u)
-threads = [Thread(target=act, args=[client, 'client{}'.format(i), u]) for i, client in enumerate(clients)]
-for thread in threads: thread.start()
-for thread in threads: thread.join()
-for client in clients: print(client.uuid)
+
+
+def add_dummy_friends(u, n):
+    """Script function to add n: int friends to u: UUID"""
+    clients = [Client(None, True, 'poke.zachlef.in') for _ in range(n)]
+    def act(client, name, friend):
+        client.register(name)
+        client.add_friend(u)
+    threads = [Thread(target=act, args=[client, 'client{}'.format(i), u]) for i, client in enumerate(clients)]
+    for thread in threads: thread.start()
+    for thread in threads: thread.join()
+    for client in clients: print(client.uuid)
+
+
+if __name__ == '__main__':
+    u = input('Your UUID> ')
+    n = int(input('Number of friends to generate/add> '))
+    add_dummy_friends(u, n)
+
--- tests/delete_all_friends.py
+++ tests/delete_all_friends.py
@@ -3,10 +3,16 @@ from uuid import UUID
 from threading import Thread
 import json
 from cli_client import Client
-u = input('Your UUID> ')
-client = Client(u, True, 'poke.zachlef.in')
-friends = json.loads(client.update())['friends']
-client.friends = [UUID(uuid) for name, uuid in friends]
-for name, uuid in friends:
-    Thread(target=lambda: client.delete_friend(UUID(uuid))).start()
-    print('Remove', name, uuid)
+
+def delete_all_friends(u):
+    """Script function to delete all friends of u: UUID"""
+    client = Client(u, True, 'poke.zachlef.in')
+    friends = json.loads(client.update())['friends']
+    client.friends = [UUID(uuid) for name, uuid in friends]
+    for name, uuid in friends:
+        Thread(target=lambda: client.delete_friend(UUID(uuid))).start()
+        print('Remove', name, uuid)
+
+if __name__ == "__main":
+    u = input('Your UUID> ')
+    delete_all_friends(u)
